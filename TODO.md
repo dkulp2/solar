e899b2406e5f0ee97f0718b8a4ca5d30ac9cedc5
David Kulp <dkulp@dizz.org>
2015-02-11 12:00:26 -0500

    *** empty log message ***

---
+++ TODO.md
@@ -0,0 +1,5 @@
+* Use find() in thisContract instead of underscore functions.
+* Create a contracts collection in MongoDB on server side.
+* replace static redirect with something that uses Contracts.findOne(). Can I call the route by name and pass params instead of creating a new URL?
+* Add a form.  
+* Figure out how to style "properly".
---
+++ server/collections.js
@@ -0,0 +1,29 @@
+// On the server, profiles are just a different view of Meteor.users
+// that includes additional data regarding login to WMECO.  Password
+// should be stored on server, but not accessible by client.  It can
+// be reset.
+Meteor.publish("profiles", function () {
+    if (this.userId) {
+	return Meteor.users.find({_id: this.userId},
+				 {fields: { 'emails': 1, 'profile': 1, 'accountId': 1}});
+    } else {
+	this.ready();
+    }
+});
+
+// TODO: create the collection in MongoDB so that we can define the indices
+Meteor.publish("contracts", function() {
+    if (this.userId) {
+        return Contracts.find({acctId: this.userId});
+    } else {
+        this.ready();
+    }
+});
+
+Meteor.publish("customers", function() {
+    if (this.userId) {
+        return Customers.find({acctId: this.userId});
+    } else {
+        this.ready();
+    }
+});
